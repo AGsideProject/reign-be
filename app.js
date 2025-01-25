c78261d1da363f6ee9d54e4db26a631b2ac9b4aa
aldomarcelino <aldo115marcelino@gmail.com>
2025-01-25 13:46:05 +0700

    fix cors

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const cookieParser = require("cookie-parser");
 
 const corsOptions = {
   origin: process.env.CLIENT_URL,
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true,
 };
 
