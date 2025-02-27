ca5a23381da95f077076b61aa482b15337095580
aldomarcelino <aldo115marcelino@gmail.com>
2025-02-27 22:00:05 +0700

    uodate config

--- config/config.js
+++ config/config.js
@@ -2,28 +2,30 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    dialect: "postgres",
+    username: process.env.DB_USERNAME || "postgres",
+    password: process.env.DB_PASSWORD || "postgres",
+    database: process.env.DB_DATABASE || "reign_dev",
+    host: process.env.DB_HOST || "localhost",
+    dialect: process.env.DB_DIALECT || "postgres",
   },
   test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    username: process.env.DB_USERNAME || "root",
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_DATABASE || "database_test",
+    host: process.env.DB_HOST || "127.0.0.1",
+    dialect: process.env.DB_DIALECT || "mysql",
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    dialect: "postgres",
+    username: process.env.DB_USERNAME || "reign_db_prod_user",
+    password: process.env.DB_PASSWORD || "52zLeLln1lMuSQobq4STa8RQ94WhNaVU",
+    database: process.env.DB_DATABASE || "reign_db_prod_4yxh",
+    host:
+      process.env.DB_HOST ||
+      "dpg-cucvdthopnds73amnnv0-a.singapore-postgres.render.com",
+    dialect: process.env.DB_DIALECT || "postgres",
     dialectOptions: {
       ssl: {
-        require: process.env.DB_SSL === "true",
+        require: true,
         rejectUnauthorized: false,
       },
     },
