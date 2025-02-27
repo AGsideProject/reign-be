aced54aa57b1ec70689aa6c64d76e22d93e96933
aldomarcelino <aldo115marcelino@gmail.com>
2025-02-27 22:18:39 +0700

    back to initial config

--- models/index.js
+++ models/index.js
@@ -1,14 +1,12 @@
 "use strict";
 
-require("dotenv").config(); // Load .env variables
 const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
 const process = require("process");
-
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/config.js")[env];
+const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
 
 let sequelize;
@@ -16,15 +14,10 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(
-    process.env.DB_DATABASE || config.database,
-    process.env.DB_USERNAME || config.username,
-    process.env.DB_PASSWORD || config.password,
-    {
-      host: process.env.DB_HOST || config.host,
-      dialect: process.env.DB_DIALECT || config.dialect,
-      dialectOptions: config.dialectOptions || {},
-      logging: process.env.DB_LOGGING === "true" ? console.log : false,
-    }
+    config.database,
+    config.username,
+    config.password,
+    config
   );
 }
 
